b09939aac204cba977ac6ca8d9ab7e6db5b78aff
Aadarsh <as7128@srmist.edu.in>
2024-12-11 23:29:40 +0530

    Updated Sidebar.js

--- src/All Components/SideBar.js
+++ src/All Components/SideBar.js
@@ -194,15 +194,12 @@ const SideBar = ( ) => {
               Coming Soon
             </span>
           </ListItem>
-          <ListItem onClick={() => alert("Coming Soon!")}  className={`border-b-0 p-3 ${open === 3 ? 'bg-blue-gray-50' : 'hover:bg-blue-gray-50'}`}>
-            <ListItemPrefix>
-              <NewspaperIcon className="h-5 w-5 text-black"/>
-            </ListItemPrefix>
-            DPS.
-            <span className="inline-flex items-center rounded-md ml-1 bg-black px-2 py-1.5 text-xs font-bold text-white ring-1 ring-inset ring-green-600/20">
-              Coming Soon
-            </span>
-          </ListItem>
+          <ListItem onClick={() => navigate("/DPS")} className={`border-b-0 p-3 ${open === 3 ? 'bg-blue-gray-50' : 'hover:bg-blue-gray-50'}`}>
+                <ListItemPrefix>
+                  <NewspaperIcon className="h-5 w-5 text-black" />
+                </ListItemPrefix>
+                DPS.
+              </ListItem>
           <ListItem onClick={() => navigate("/profile")} className={`border-b-0 p-3 ${open === 3 ? 'bg-blue-gray-50' : 'hover:bg-blue-gray-50'}`}>
             <ListItemPrefix>
               <UserCircleIcon className="h-5 w-5 text-black"/>
