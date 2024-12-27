165d8592ece4df8fd92bb86e3f1e7de8225d0926
Aadarsh <as7128@srmist.edu.in>
2024-12-27 19:21:02 +0530

    Updated Busy

--- src/All Components/LeadsDistribution.js
+++ src/All Components/LeadsDistribution.js
@@ -493,7 +493,12 @@ const LeadsDistribution = () => {
                                   ))}
                                 </select>
 
-                                {(updatedLeads[id]?.status === "Follow Up" || updatedLeads[id]?.status === "Not Interested" || status === "Follow Up" || status === "Not Interested") && (
+                                {(updatedLeads[id]?.status === "Follow Up" || 
+                                  updatedLeads[id]?.status === "Not Interested" || 
+                                  updatedLeads[id]?.status === "Busy" || 
+                                  status === "Follow Up" || 
+                                  status === "Not Interested" ||
+                                  status === "Busy") && (
                                   <>
                                     <button
                                       className='text-white bg-black hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800' onClick={handleOpenNote} variant="gradient">
